62cda861c5edb024e50d415a3ab4da62f6dba5e8
林岑影 <lincenying@qq.com>
2024-01-22 16:06:47 +0800

    feat: markdown code高亮改成prism

--- src/app.vue
+++ src/app.vue
@@ -8,12 +8,13 @@
 <script setup lang="ts">
 import { appName } from '~/constants'
 
+import './assets/css/github-markdown.css'
+import './assets/css/hljs/googlecode.css'
 import '@kangc/v-md-editor/lib/style/base-editor.css'
-import '@kangc/v-md-editor/lib/theme/style/github.css'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
 import 'vue-loading-overlay/dist/css/index.css'
 import 'element-plus/es/components/message/style/css'
 import 'element-plus/es/components/message-box/style/css'
-import './assets/css/hljs/googlecode.css'
 import './assets/scss/style.scss'
 
 useHead({
--- src/plugins/md-editor.ts
+++ src/plugins/md-editor.ts
@@ -1,11 +1,24 @@
 import VueMarkdownEditor from '@kangc/v-md-editor'
-import vuePressTheme from '@kangc/v-md-editor/lib/theme/github'
+import vuePressTheme from '@kangc/v-md-editor/lib/theme/vuepress'
 
-import hljs from 'highlight.js'
+// Prism
+import Prism from 'prismjs'
+
+// highlight code
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-twig'
+import 'prismjs/components/prism-css'
+import 'prismjs/components/prism-sass'
+import 'prismjs/components/prism-less'
+import 'prismjs/components/prism-bash'
 
 export default defineNuxtPlugin((nuxtApp) => {
     VueMarkdownEditor.use(vuePressTheme, {
-        Hljs: hljs,
+        Prism,
     })
 
     nuxtApp.vueApp.use(VueMarkdownEditor)
